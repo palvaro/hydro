c254fe1ee26f2256779f919fc32ec6413b53943e
Joe Hellerstein <jmh@berkeley.edu>
2025-04-03 14:56:09 -0700

    feat(hydro): chat and echo examples (#1824)
    These are so simple and similar it merits a nice walk-through discussion
    in the docs.

---
+++ hydro_test/examples/chat.rs
@@ -0,0 +1,25 @@
+use hydro_deploy::Deployment;
+use hydro_lang::deploy::TrybuildHost;
+
+#[tokio::main]
+async fn main() {
+    let mut deployment = Deployment::new();
+    let builder = hydro_lang::FlowBuilder::new();
+    let num_clients: u32 = 3;
+
+    let (server, clients) = hydro_test::cluster::chat::chat_server(&builder);
+
+    let _nodes = builder
+        .with_process(&server, TrybuildHost::new(deployment.Localhost()))
+        .with_cluster(
+            &clients,
+            (0..num_clients).map(|_| TrybuildHost::new(deployment.Localhost())),
+        )
+        .deploy(&mut deployment);
+
+    deployment.deploy().await.unwrap();
+
+    deployment.start().await.unwrap();
+
+    tokio::signal::ctrl_c().await.unwrap();
+}
