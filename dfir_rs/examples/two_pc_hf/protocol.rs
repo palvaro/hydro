d192c5eafe8afe6d716b5213f3a9544e0b427bc4
Shadaj Laddad <shadaj@users.noreply.github.com>
2025-09-18 13:43:31 -0700

    fix(hydro_lang): update backtrace snapshots for Rust 1.90 and remove unused types (#2122)
    The line number of `core::ops::function::FnOnce::call_once` in the
    standard library changed.

--- dfir_rs/examples/two_pc_hf/protocol.rs
+++ dfir_rs/examples/two_pc_hf/protocol.rs
@@ -1,28 +1,6 @@
 use dfir_rs::DemuxEnum;
 use serde::{Deserialize, Serialize};
 
-#[derive(PartialEq, Eq, Clone, Serialize, Deserialize, Debug, Hash, Copy)]
-pub enum MsgType {
-    Prepare,
-    Commit,
-    Abort,
-    AckP2,
-    End,
-    Ended,
-}
-
-#[derive(PartialEq, Eq, Clone, Serialize, Deserialize, Debug)]
-pub struct CoordMsg {
-    pub xid: u16,
-    pub mtype: MsgType,
-}
-/// Member Response
-#[derive(PartialEq, Eq, Clone, Serialize, Deserialize, Debug)]
-pub struct SubordResponse {
-    pub xid: u16,
-    pub mtype: MsgType,
-}
-
 #[derive(PartialEq, Eq, Clone, Serialize, Deserialize, Debug, Hash, Copy, DemuxEnum)]
 pub enum Msg {
     Prepare(u16),
