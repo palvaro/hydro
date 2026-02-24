2a88d2bceae604e7a1c568318b1cf0a3f799082d
Shadaj Laddad <shadaj@users.noreply.github.com>
2026-02-24 09:27:54 -0800

    fix(hydro_test): fix broken embedded test after merge (#2604)

--- hydro_test/src/embedded/m2m_broadcast.rs
+++ hydro_test/src/embedded/m2m_broadcast.rs
@@ -1,4 +1,4 @@
-use hydro_lang::live_collections::stream::NoOrder;
+use hydro_lang::live_collections::stream::{NoOrder, TotalOrder};
 use hydro_lang::location::MemberId;
 use hydro_lang::prelude::*;
 
@@ -14,7 +14,7 @@ pub fn m2m_broadcast<'a>(
         .location()
         .source_cluster_members(dst)
         .entries()
-        .assume_ordering(nondet!(/** test */))
+        .assume_ordering::<TotalOrder>(nondet!(/** test */))
         .for_each(q!(|_| {}));
 
     input
